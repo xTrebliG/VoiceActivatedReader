8e17d0fcb5c9f3fcb05fc13fd1a3aea43ce35f46
xTrebliG <gil@crankshiftnyc.com>
2015-03-31 15:00:51 -0400

    Add voice to landing page for heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( parallax.js )
 Rails.application.config.assets.precompile += %w( parallax.css )
 Rails.application.config.assets.precompile += %w( _bootstrap.css )
+Rails.application.config.assets.precompile += %w( annyang.min.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
